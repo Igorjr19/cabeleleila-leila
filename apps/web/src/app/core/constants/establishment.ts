c5d6ec79d5fbad950c0dcd900a7eaba46061cb31
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-05-09 18:12:50 -0300

    fix: update phone placeholder

--- apps/web/src/app/core/constants/establishment.ts
+++ apps/web/src/app/core/constants/establishment.ts
@@ -1,5 +1,5 @@
 import { environment } from '../../../environments/environment';
 
 export const ESTABLISHMENT_ID = environment.establishmentId;
-export const SALON_PHONE = '(XX) XXXX-XXXX';
+export const SALON_PHONE = '(14) 99538-6604';
 export const SALON_NAME = 'Salão de Beleza da Leila';
